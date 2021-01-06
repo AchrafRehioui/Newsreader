1b9bb313d0e98f1e55b7723c38ba02b81fe5a7ee
Achraf <achraf.rehioui.1@gmail.com>
2021-01-06 20:50:54 +0100

    creating hnApi.js service file

---
+++ src/services/hnApi.js
@@ -0,0 +1,9 @@
+import axios from 'axios';
+
+
+export const baseUrl = ` https://hacker-news.firebaseio.com/v0/`;
+
+export const newStoriesUrl = `${baseUrl}newStories.json`;
+
+
+export const storyUrl = `${baseUrl}item/`;
