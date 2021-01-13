13f036c0d249cc7f594062d59cadd64e1d2f1e3e
Achraf <achraf.rehioui.1@gmail.com>
2021-01-13 16:56:49 +0100

    adding a StoryWrapper to story.js component

--- src/styles/StoryStyles.js
+++ src/styles/StoryStyles.js
@@ -18,3 +18,16 @@ border-top: 1px solid #cccccc;
 }
 
 `;
+
+// export const StoryTitle = styled.h1`
+//     margin-bottom: 5px;
+//     font-size: 18px;
+//     line-height: 1.8;
+//     margin: 0;
+//     text-decoration: none;
+
+//     a {
+//         color: #2e2e2c;
+//     }
+
+// `;
