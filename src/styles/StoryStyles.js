95e704711ea57ea0d8ec2d49fda7d3964cc581b6
Achraf <achraf.rehioui.1@gmail.com>
2021-01-13 17:15:08 +0100

    adding styling for StoryMeta

--- src/styles/StoryStyles.js
+++ src/styles/StoryStyles.js
@@ -19,15 +19,33 @@ border-top: 1px solid #cccccc;
 
 `;
 
-// export const StoryTitle = styled.h1`
-//     margin-bottom: 5px;
-//     font-size: 18px;
-//     line-height: 1.8;
-//     margin: 0;
-//     text-decoration: none;
+export const StoryTitle = styled.h1`
+    margin-bottom: 5px;
+    font-size: 18px;
+    line-height: 1.8;
+    margin: 0;
+    text-decoration: none;
+    a {
+        color: #2e2e2c;
+        background-color: #f8dc3d;
+        text-decoration: none;
 
-//     a {
-//         color: #2e2e2c;
-//     }
+    }
+`;
 
-// `;
+export const StoryMeta = styled.div`
+  font-style: italic;
+
+  > span:first-child {
+    margin-right: 10px;
+  }
+
+  > span:not(:first-child):before {
+    content: '•'
+    margin: 0 7px;
+  }
+  
+  .story__meta-bold {
+    font-weight: bold;
+  }
+`;
