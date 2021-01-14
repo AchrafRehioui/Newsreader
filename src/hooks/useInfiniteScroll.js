3ee29cf21b437d2aa94a091771767102b24f31f6
Achraf <achraf.rehioui.1@gmail.com>
2021-01-14 20:17:15 +0100

    adding debounce for scolling

--- src/hooks/useInfiniteScroll.js
+++ src/hooks/useInfiniteScroll.js
@@ -1,5 +1,6 @@
 import { useState, useEffect } from 'react';
 import { STORY_INCREMENT, MAX_STORIES } from '../constants';
+import { debounce } from '../utils/debounce';
 
 
 export const useInfiniteScroll = () => {
@@ -7,7 +8,7 @@ export const useInfiniteScroll = () => {
     const [loading, setLoading] = useState(false);
     const [count, setCount] = useState(STORY_INCREMENT);
 
-    const handleScroll = () => {
+    const handleScroll = debounce(() => {
 
         if (
             window.innerHeight + document.documentElement.scrollTop !==
@@ -18,19 +19,20 @@ export const useInfiniteScroll = () => {
         }
 
         setLoading(true);
-    };
+    }, 500);
+
 
     useEffect(() => {
         if (!loading) return;
-    
+
         if (count + STORY_INCREMENT >= MAX_STORIES) {
-          setCount(MAX_STORIES);
+            setCount(MAX_STORIES);
         } else {
-          setCount(count + STORY_INCREMENT);
+            setCount(count + STORY_INCREMENT);
         }
-    
+
         setLoading(false);
-      }, [loading]);
+    }, [loading]);
 
     useEffect(() => {
 
