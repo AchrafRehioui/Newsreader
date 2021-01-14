593e254ad220a5f19949b2dee2e247e15a3c1a7b
Achraf <achraf.rehioui.1@gmail.com>
2021-01-14 20:51:02 +0100

    adding a fixture folder with fixtures for tests

---
+++ src/fixtures/index.js
@@ -0,0 +1,18 @@
+export const singularStory = {
+    by: 'achraf achraf',
+    id: 1,
+    time: 1567209822,
+    title: 'Tarnished: Google Responds',
+    url: 'https://something.com/hackernewstut',
+};
+
+export const storyIds = [1];
+
+export const emptySingularStory = {
+
+    by: undefined,
+    id: undefined,
+    time: undefined,
+    url: undefined,
+
+};
