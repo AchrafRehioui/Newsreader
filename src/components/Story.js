1aeb2397a282fff3dfd2f0338c084a1b723698af
Achraf <achraf.rehioui.1@gmail.com>
2021-01-14 13:31:48 +0100

    constants to be used by InfiniteScroll for per page items

--- src/components/Story.js
+++ src/components/Story.js
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState, useEffect, memo } from 'react';
 import { getStory } from '../services/hnApi';
 import { StoryWrapper, StoryMeta, StoryMetaElement, StoryTitle } from '../styles/StoryStyles';
 import { mapTime } from '../mappers/mapTime';
